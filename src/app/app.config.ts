133d4e847ce97b5b2a9d04426c2c595db7a6eff0
WencyOpiso <opisowc@students.nu-fairview.edu.ph>
2026-01-19 15:12:52 +0800

    fix: Update angular.json and package.json for deployment configuration and add angular-cli-ghpages dependency; modify app.config.ts for HashLocationStrategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ]
 };
